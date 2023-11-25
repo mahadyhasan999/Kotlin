ef89b7a3755d2015c1189faab9aa4760be246e2e
Mahady Hasan Miraz <mhmiraz30@gmail.com>
2023-11-25 13:26:38 +0600

    November25

--- HW_leapYear.kt
+++ HW_leapYear.kt
@@ -5,7 +5,7 @@ fun main() {
     print("Enter a year: ")
     val year = scanner.nextInt()
 //    2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, and 2048
-    if (year % 4 == 0) {
+    if (year % 400 == 0 || year % 100 == 0 || year % 4 == 0) {
         println("$year is a leap year.")
     } else {
         println("$year is not a leap year.")
